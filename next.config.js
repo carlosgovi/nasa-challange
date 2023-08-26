00f8b34d55c4d9a2fef7b898dc747d56d5a619fc
carlosgovi <carlosgovi.dev@gmail.com>
2023-08-25 21:15:53 -0300

    first commit challange

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["mars.jpl.nasa.gov", "mars.nasa.gov"], //dominio de la API de la NASA
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,200 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import {
+  Box,
+  FormControl,
+  ImageList,
+  ImageListItem,
+  ImageListItemBar,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+
+import StarOutlineIcon from "@mui/icons-material/StarOutline";
+import StarIcon from "@mui/icons-material/Star";
+import Header from "./components/header";
+import { fetchAPI } from "../../service/api";
+import { Triangle } from "react-loader-spinner";
+import { useLocalStorageArray } from "../../service/hooks";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [cameras, setCameras] = useState([]);
+  const [photos, setPhotos]: any = useState([]);
+  const [Rovers, setRovers] = useState("curiosity");
+  const [cameraSelected, setCameraSelected] = useState("");
+
+  const [items, setItems] = useLocalStorageArray("items");
+
+  const fetchMorePhotos = async () => {
+    const nextPage: number = currentPage + 1;
+    const newData = await fetchAPI({ page: nextPage, rovers: Rovers });
+
+    setPhotos([...photos, ...newData?.photos]);
+    setCurrentPage(nextPage);
+  };
+
+  const handleScroll = () => {
+    const isNearBottom =
+      window.innerHeight + window.scrollY >= document.body.offsetHeight - 200;
+
+    if (isNearBottom) {
+      fetchMorePhotos();
+    }
+  };
+  const handleChangeRovers = (event: SelectChangeEvent) => {
+    setRovers(event.target.value as string);
+    setCurrentPage(1);
+    setCameraSelected("");
+  };
+  const handleChangeCamera = (event: SelectChangeEvent) => {
+    setCameraSelected(event.target.value as string);
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchAPI({ page: 1, rovers: Rovers });
+      setPhotos(data?.photos || []);
+
+      const arrayCamaras =
+        data?.photos.map((item: any) => {
+          return item.camera.name;
+        }) || [];
+      const uniqueArray: any = Array.from(new Set(arrayCamaras));
+      setCameras(uniqueArray);
+      setIsLoading(false);
+    };
+    fetchData();
+  }, [Rovers]);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div>
+      <Header />
+      <Box sx={{ minWidth: 120, backgroundColor: "white" }}>
+        <FormControl sx={{ m: 1, minWidth: 120 }}>
+          <InputLabel id="demo-simple-select-label">Rovers</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={Rovers}
+            label="Rovers"
+            onChange={handleChangeRovers}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+            <MenuItem value="curiosity">Curiosity</MenuItem>
+            <MenuItem value="opportunity">Opportunity</MenuItem>
+            <MenuItem value="spirit">Spirit</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl sx={{ m: 1, minWidth: 120 }}>
+          <InputLabel id="demo-simple-select-camaras">Cameras</InputLabel>
+          <Select
+            labelId="demo-simple-select-camaras"
+            id="demo-simple-select-camaras"
+            value={"all" === cameraSelected ? "" : cameraSelected}
+            label="Cameras"
+            onChange={handleChangeCamera}
+          >
+            {cameras?.map((item: any) => (
+              <MenuItem key={item} value={item}>
+                {item}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Box>
+      <div>
+        {isLoading ? (
+          <Triangle
+            height="200"
+            width="200"
+            color="#ffffffb8"
+            ariaLabel="triangle-loading"
+            wrapperStyle={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              margin: "20%",
+            }}
+            visible={true}
+          />
+        ) : (
+          <ImageList variant="masonry" cols={3} gap={8}>
+            {cameraSelected
+              ? photos
+                  ?.filter((item: any) => item.camera.name === cameraSelected)
+                  .map((item: any) => {
+                    return (
+                      <ImageListItem key={item.id}>
+                        <Image
+                          src={`${item.img_src}?w=248&fit=crop&auto=format`}
+                          alt={item.id}
+                          layout="responsive"
+                          width={248}
+                          height={160}
+                        />
+
+                        <ImageListItemBar
+                          title={item.rover.name}
+                          subtitle={item.camera.full_name}
+                          actionIcon={
+                            <IconButton
+                              sx={{ color: "rgba(255, 255, 255, 0.54)" }}
+                              aria-label={`info about ${item.rover.name}`}
+                            >
+                              <StarOutlineIcon />
+                            </IconButton>
+                          }
+                        />
+                      </ImageListItem>
+                    );
+                  })
+              : photos?.map((item: any) => {
+                  return (
+                    <ImageListItem key={item.id}>
+                      <Image
+                        src={`${item.img_src}?w=248&fit=crop&auto=format`}
+                        alt={item.id}
+                        loading="lazy"
+                        layout="responsive"
+                        width={248}
+                        height={160}
+                      />
+
+                      <ImageListItemBar
+                        title={item.rover.name}
+                        subtitle={item.camera.full_name}
+                        actionIcon={
+                          <IconButton
+                            sx={{ color: "rgba(255, 255, 255, 0.54)" }}
+                            onClick={() => {
+                              setItems([...items, item.id]);
+                            }}
+                          >
+                            {items?.includes(item.id) ? (
+                              <StarIcon />
+                            ) : (
+                              <StarOutlineIcon />
+                            )}
+                          </IconButton>
+                        }
+                      />
+                    </ImageListItem>
+                  );
+                })}
+          </ImageList>
+        )}
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
